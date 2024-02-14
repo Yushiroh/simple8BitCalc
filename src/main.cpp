2993c14ffba94ec8ff51fcbd21111e8a8c099eb5
Yushiroh <Yushiroh.13@gmail.com>
2024-02-15 01:06:07 +0800

    setup commit

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+
+const int ledStore1 = 7;
+const int ledStore2 = 8;
+
+const int dip1 = 9;
+const int dip2 = 10;
+const int dip3 = 11;
+const int dip4 = 12;
+
+const int storeBut = 13;
+
+const int led1 = A4;
+const int led2 = A3;
+const int led3 = A2;
+const int led4 = A1;
+const int led5 = A0;
+
+int leds[] = {led1,led2,led3,led4,led5,ledStore1,ledStore2};
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(ledStore1, OUTPUT);
+  pinMode(ledStore2, OUTPUT);
+
+  pinMode(dip1, INPUT);
+  pinMode(dip2, INPUT);
+  pinMode(dip3, INPUT);
+  pinMode(dip4, INPUT);
+
+  pinMode(storeBut, INPUT);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+
+}
+
+void loop() {
+
+ for(int i = 0; i < 7; i++){
+
+  digitalWrite(leds[i], HIGH);
+  delay(500);
+  digitalWrite(leds[i], LOW);
+  
+ }
+
+  Serial.print(digitalRead(dip1));
+  Serial.print(digitalRead(dip2));
+  Serial.print(digitalRead(dip3));
+  Serial.println(digitalRead(dip4));
+
+  Serial.print("Store Button State: ");
+  Serial.println(digitalRead(storeBut));
+}
+
