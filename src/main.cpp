312685d8af5cefba8da04231d5da2824e3b1689a
Yushiroh <Yushiroh.13@gmail.com>
2024-02-15 09:33:53 +0800

    working state manager

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,13 @@ const int led5 = A0;
 
 int leds[] = {led5,led4,led3,led2,led1};
 
+int bitArray[4];
+
+int var1[4];
+int var2[4];
+
+int progState = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -40,9 +47,25 @@ void setup() {
 }
 
 void loop() {
-  
+
   int inputStates[] = {digitalRead(dip1), digitalRead(dip2), digitalRead(dip3), digitalRead(dip4)};
   
+  int buttonState = digitalRead(storeBut);
+  delay(100);
+
+  Serial.println(progState);
+
+  if(buttonState == 1){
+    progState++;
+
+    progState == 3 ? progState = 0 : Serial.println("Button Pressed");
+
+  }else{
+
+
+    
+  }
+
   for(int ledPos = 0; ledPos < 4; ledPos++){
 
     if(inputStates[ledPos] == 1){
@@ -51,6 +74,7 @@ void loop() {
       digitalWrite(leds[ledPos], LOW);
     }
   }
+
 
 }
 
